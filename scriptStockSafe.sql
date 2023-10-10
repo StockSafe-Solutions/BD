355456c6c7400901054ac41485632d8db0c62326
gba-exe <gabriel.baraujo@sptech.school>
2023-10-10 15:17:02 -0300

    refactor(): arrumando os inserts.

--- scriptStockSafe.sql
+++ scriptStockSafe.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS StockSafe;
 USE StockSafe ;
 
 CREATE TABLE IF NOT EXISTS tb_funcionario (
-  id_funcionario INT NOT NULL,
+  id_funcionario INT NOT NULL AUTO_INCREMENT,
   nome VARCHAR(125) NOT NULL,
   funcao VARCHAR(75) NOT NULL,
   data_nascimento DATE NOT NULL,
@@ -14,14 +14,15 @@ CREATE TABLE IF NOT EXISTS tb_funcionario (
   
 
 CREATE TABLE IF NOT EXISTS tb_servidor (
-  id_servidor INT NOT NULL,
+  id_servidor INT NOT NULL AUTO_INCREMENT,
   codigo CHAR(6) NOT NULL,
   armazenamento_total DECIMAL(4,1) NULL,
   armazenamento_usado DECIMAL(4,1) NULL,
   id_autenticador INT NOT NULL,
   PRIMARY KEY (id_servidor, id_autenticador),
   FOREIGN KEY (id_autenticador) REFERENCES funcionario (id_funcionario)
-    );
+    ) 
+    AUTO_INCREMENT = 2000;
 
 CREATE TABLE IF NOT EXISTS tb_tipo_componente (
   id_tipo INT NOT NULL AUTO_INCREMENT,
@@ -32,7 +33,7 @@ CREATE TABLE IF NOT EXISTS tb_tipo_componente (
   
 
 CREATE TABLE IF NOT EXISTS tb_registro (
-  id_registro INT NOT NULL,
+  id_registro INT NOT NULL AUTO_INCREMENT,
   fk_servidor INT NOT NULL,
   data_hora DATETIME NOT NULL DEFAULT now(),
   pacotes_enviados TINYINT NULL,
@@ -48,7 +49,7 @@ CREATE TABLE IF NOT EXISTS tb_registro (
     
     
 CREATE TABLE IF NOT EXISTS tb_opcao (
-  id_opcao INT NOT NULL,
+  id_opcao INT NOT NULL AUTO_INCREMENT,
   banda_larga SMALLINT NOT NULL,
   taxa_transferencia DECIMAL(10,2) NOT NULL,
   PRIMARY KEY (id_opcao)
@@ -61,44 +62,44 @@ CREATE TABLE IF NOT EXISTS tb_opcao (
   
 INSERT INTO funcionario VALUES (1, 'Danilo', 'Analista', '2005-07-11', null, 'danilo@gmal.com', 'urubu100');
 
-INSERT INTO servidor (id_servidor, codigo, armazenamento_total, armazenamento_usado, id_autenticador) VALUES
-		(1, 'SRV001', 500.5, 250.2, 1),
-		(2, 'SRV002', 750.0, 375.5, 1),
-        (3, 'SRV003', 300.3, 150.1, 1),
-        (4, 'SRV004', 900.0, 500.0, 1),
-        (5, 'SRV005', 800.8, 400.4, 1),
-        (6, 'SRV006', 600.6, 300.3, 1),
-        (7, 'SRV007', 900.9, 450.4, 1),
-        (8, 'SRV008', 400.4, 200.2, 1),
-        (9, 'SRV009', 700.7, 350.3, 1),
-        (10, 'SRV010', 980.0, 600.0, 1),
-        (11, 'SRV011', 450.4, 225.2, 1),
-        (12, 'SRV012', 850.8, 425.4, 1),
-        (13, 'SRV013', 250.2, 125.1, 1),
-        (14, 'SRV014', 950.9, 475.5, 1),
-        (15, 'SRV015', 550.5, 275.2, 1),
-        (16, 'SRV016', 950.0, 550.0, 1),
-        (17, 'SRV017', 350.3, 175.1, 1),
-        (18, 'SRV018', 650.6, 325.3, 1),
-        (19, 'SRV019', 150.1, 75.0, 1),
-		(20, 'SRV020', 400.4, 200.2, 1);
+INSERT INTO servidor (codigo, armazenamento_total, armazenamento_usado, id_autenticador) VALUES
+		('SRV001', 500.5, 250.2, 1),
+		('SRV002', 750.0, 375.5, 1),
+        ('SRV003', 300.3, 150.1, 1),
+        ('SRV004', 900.0, 500.0, 1),
+        ('SRV005', 800.8, 400.4, 1),
+        ('SRV006', 600.6, 300.3, 1),
+        ('SRV007', 900.9, 450.4, 1),
+        ('SRV008', 400.4, 200.2, 1),
+        ('SRV009', 700.7, 350.3, 1),
+        ('SRV010', 980.0, 600.0, 1),
+        ('SRV011', 450.4, 225.2, 1),
+        ('SRV012', 850.8, 425.4, 1),
+        ('SRV013', 250.2, 125.1, 1),
+        ('SRV014', 950.9, 475.5, 1),
+        ('SRV015', 550.5, 275.2, 1),
+        ('SRV016', 950.0, 550.0, 1),
+        ('SRV017', 350.3, 175.1, 1),
+        ('SRV018', 650.6, 325.3, 1),
+        ('SRV019', 150.1, 75.0, 1),
+		('SRV020', 400.4, 200.2, 1);
 
 INSERT INTO tipo_componente (nome, unidade_medida) VALUES ('RAM', '%'), ('Taxa de Transferência', 'Gb/s'), ('CPU', '%');
 
-INSERT INTO registro (id_registro, fk_servidor, data_hora, perda_de_pacotes, uso_cpu, uso_ram, taxa_transferencia, fk_tipo) VALUES 
-		(1, 1, NOW(), 5, 20, 30, 100, 1),
-		(2, 2, NOW(), 2, 15, 25, 80, 1),
-        (3, 3, NOW(), 0, 10, 20, 60, 1),
-        (4, 4, NOW(), 3, 18, 28, 90, 2),
-        (5, 5, NOW(), 1, 12, 22, 70, 2),
-        (6, 6, NOW(), 4, 16, 26, 85, 2),
-        (7, 7, NOW(), 2, 14, 24, 75, 3),
-        (8, 8, NOW(), 0, 10, 20, 60, 3),
-        (9, 9, NOW(), 5, 22, 32, 105, 3),
-        (10, 10, NOW(), 1, 12, 22, 70, 1),
-        (11, 11, NOW(), 3, 18, 28, 90, 1),
-        (12, 12, NOW(), 2, 15, 25, 80, 2),
-        (13, 13, NOW(), 0, 10, 20, 60, 2),
-        (14, 14, NOW(), 4, 16, 26, 85, 3),
-		(15, 15, NOW(), 3, 18, 28, 90, 3);
+INSERT INTO registro (fk_servidor, data_hora, uso_ram, fk_tipo) VALUES 
+		(2001, NOW(), 30, 1),
+		(2002, NOW(), 25, 1),
+        (2003, NOW(), 20, 1),
+        (2004, NOW(), 28, 2),
+        (2005, NOW(), 22, 2),
+        (2006, NOW(), 26, 2),
+        (2007, NOW(), 24, 3),
+        (2008, NOW(), 20, 3),
+        (2009, NOW(), 32, 3),
+        (2010, NOW(), 22, 1),
+        (2011, NOW(), 28, 1),
+        (2012, NOW(), 25, 2),
+        (2013, NOW(), 20, 2),
+        (2014, NOW(), 26, 3),
+		(2015, NOW(), 28, 3);
 
