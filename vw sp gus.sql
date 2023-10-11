41676bb56afba3e6d8553c360b78f0afff906f8d
PereiraGus <gustavo.oito@hotmail.com>
2023-10-11 15:00:25 -0300

    fix(vw sp gus.sql): Fixes da sp_cpu_maquina.

--- vw sp gus.sql
+++ vw sp gus.sql
@@ -1,11 +1,10 @@
 DELIMITER $$
 CREATE PROCEDURE sp_cpu_maquina(v_fk_servidor INT)
 	BEGIN
-		SELECT fk_servidor, uso_cpu FROM registro
-			WHERE fk_servidor = v_fk_servidor;
+		SELECT fk_servidor, uso_cpu FROM tb_registro
+			WHERE fk_servidor = 2001 AND uso_cpu IS NOT NULL;
     END
 $$ DELIMITER ;
-
-CALL sp_cpu_maquina(5);
+CALL sp_cpu_maquina(1);
 
 SELECT perda_de_pacotes FROM registro;
