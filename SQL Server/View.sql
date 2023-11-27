c7a60a991d92cdd3b04509b756f1da1adbd0e1ae
JuliaOJustino <stephany.justino@sptech.school>
2023-11-27 20:27:21 -0300

    feat(): Views

--- SQL Server/View.sql
+++ SQL Server/View.sql
@@ -1,112 +1,109 @@
-CREATE OR ALTER  VIEW vw_base_registros AS
-	SELECT 
-    r.fk_servidor,
-	data_hora,
-    tipo_cat,
-    avg(valor) as media
-		FROM tb_registro AS r
-        JOIN tb_categoria AS c on r.fk_cat = c.id_cat
-			GROUP BY fk_servidor, data_hora, c.id_cat
-go
+USE StockSafe;
+GO
+CREATE OR ALTER VIEW vw_base_registros AS
+    SELECT 
+        r.fk_servidor,
+        data_hora,
+        c.tipo_cat,
+        AVG(valor) as media
+    FROM tb_registro AS r
+    JOIN tb_categoria AS c ON r.fk_cat = c.id_cat
+    GROUP BY r.fk_servidor, data_hora, c.tipo_cat;
+GO
+CREATE OR ALTER VIEW vw_registro AS 
+	SELECT
+		fk_servidor,
+		data_hora,
+		MAX(CASE WHEN tipo_cat = 'Pacotes enviados' THEN media END) AS pacotes_enviados,
+		MAX(CASE WHEN tipo_cat = 'Taxa de transferência' THEN media END) AS taxa_de_transferencia,
+		MAX(CASE WHEN tipo_cat = 'Uso da CPU' THEN media END) AS uso_da_cpu,
+		MAX(CASE WHEN tipo_cat = 'Uso da RAM' THEN media END) AS uso_da_ram
+	FROM vw_base_registros
+	GROUP BY fk_servidor, data_hora;
+GO
+CREATE OR ALTER VIEW vw_servidor
+	AS
+	SELECT
+	s.*,
+		FORMAT(data_hora, 'dd/MMMM/yyyy', 'pt-BR') AS 'ultimaData',
+		FORMAT(data_hora, 'hh:mm', 'pt-BR') AS 'ultimoHorario'
+	FROM tb_servidor s
+	LEFT OUTER JOIN (
+		SELECT
+			fk_servidor,
+			MAX(data_hora) AS data_hora
+		FROM vw_registro
+		GROUP BY fk_servidor
+) d ON s.id_servidor = d.fk_servidor;
 
-create or alter view vw_registro as 
-select
-  fk_servidor,
-  data_hora,
-  max(case
-    when tipo_cat = 'Pacotes enviados' then media
-  end) as pacotes_enviados,
-  max(case
-    when tipo_cat = 'Taxa de transferência' then media
-  end) as taxa_de_transferência,
-  max(case
-    when tipo_cat = 'Uso da CPU' then media
-  end) as uso_da_cpu,
-  max(case
-    when tipo_cat = 'Uso da RAM' then media
-  end) as uso_da_ram
-from vw_base_registros
-group by fk_servidor, data_hora
-go
-
-select * from vw_servidor;
-
-create or alter view vw_servidor
-as
-select
-  s.*,
-  FORMAT(data_hora, 'dd/MMMM/yyyy','pt-BR') as 'ultimaData',
-  FORMAT(data_hora, 'hh:mm','pt-BR') as 'ultimoHorario'
-from tb_servidor s
-  left outer join (
-    select
-      fk_servidor,
-      max(data_hora) data_hora
-    from vw_registro
-    group by fk_servidor
-  ) d
-    on s.id_servidor = d.fk_servidor
-go
-   
-   
-create or alter view vw_cpu as
-select
-  fk_servidor,
-  avg(uso_da_cpu) uso_da_cpu,
-  FORMAT(data_hora, 'dd/MMMM/yyyy h:i') dataDados
-from vw_registro
-group by
-  FORMAT(data_hora, 'dd/MMMM/yyyy h:i'),
-  fk_servidor
-go
-
-
-create or alter view vw_cpu_geral
-as
-select
-  avg(uso_da_cpu) uso_da_cpu,
-  FORMAT(data_hora, 'dd/MM/yyyy hh:mm') dataDados
-from vw_registro
-group by FORMAT(data_hora, 'dd/MM/yyyy hh:mm')
-go
-
-
-create or alter view vw_ram as
-select
-  fk_servidor,
-  avg(uso_da_ram) uso_da_ram,
-  FORMAT(data_hora, 'dd/MM/yyyy hh:mm') dataDados
-from vw_registro
-group by
-  FORMAT(data_hora, 'dd/MM/yyyy hh:mm'),
-  fk_servidor
-go
+GO
+CREATE OR ALTER VIEW vw_cpu AS
+	SELECT
+	  fk_servidor,
+	  AVG(uso_da_cpu) AS uso_da_cpu,
+	  FORMAT(MAX(data_hora), 'dd/MMMM/yyyy h:mm') AS dataDados
+	FROM vw_registro
+	GROUP BY
+	  fk_servidor,
+	  DATEPART(YEAR, data_hora),
+	  DATEPART(MONTH, data_hora),
+	  DATEPART(DAY, data_hora),
+	  DATEPART(HOUR, data_hora),
+	  DATEPART(MINUTE, data_hora);
+GO
+CREATE OR ALTER VIEW vw_cpu_geral AS
+	SELECT
+	  AVG(uso_da_cpu) AS uso_da_cpu,
+	  FORMAT(MAX(data_hora), 'dd/MM/yyyy HH:mm') AS dataDados
+	FROM vw_registro
+	GROUP BY
+	  DATEPART(YEAR, data_hora),
+	  DATEPART(MONTH, data_hora),
+	  DATEPART(DAY, data_hora),
+	  DATEPART(HOUR, data_hora),
+	  DATEPART(MINUTE, data_hora);
+GO
+CREATE OR ALTER VIEW vw_ram AS
+	SELECT
+	  fk_servidor,
+	  AVG(uso_da_ram) AS uso_da_ram,
+	  FORMAT(MAX(data_hora), 'dd/MM/yyyy HH:mm') AS dataDados
+	FROM vw_registro
+	GROUP BY
+	  fk_servidor,
+	  DATEPART(YEAR, data_hora),
+	  DATEPART(MONTH, data_hora),
+	  DATEPART(DAY, data_hora),
+	  DATEPART(HOUR, data_hora),
+	  DATEPART(MINUTE, data_hora);
+GO
+CREATE OR ALTER VIEW vw_ram_geral AS
+	SELECT
+	  AVG(uso_da_ram) AS uso_da_ram,
+	  FORMAT(MAX(data_hora), 'dd/MM/yyyy HH:mm') AS dataDados
+	FROM vw_registro
+	GROUP BY
+	  DATEPART(YEAR, data_hora),
+	  DATEPART(MONTH, data_hora),
+	  DATEPART(DAY, data_hora),
+	  DATEPART(HOUR, data_hora),
+	  DATEPART(MINUTE, data_hora);
+GO
+CREATE OR ALTER VIEW vw_taxa_de_transferencia AS
+    SELECT
+        fk_servidor,
+        AVG(taxa_de_transferencia) AS taxa_de_transferencia,
+        data_hora
+    FROM vw_registro
+    GROUP BY data_hora, fk_servidor;
+GO
+CREATE OR ALTER VIEW vw_pacotes_enviados AS
+	SELECT
+		fk_servidor,
+		AVG(pacotes_enviados) AS pacotes_enviados,
+		data_hora
+	FROM vw_registro
+	GROUP BY data_hora, fk_servidor;
 
 
-create or alter view vw_ram_geral as
-select
-  avg(uso_da_ram) uso_da_ram,
-  FORMAT(data_hora, 'dd/MM/yyyy hh:mm') dataDados
-from vw_registro
-group by FORMAT(data_hora, 'dd/MM/yyyy hh:mm')
-go
 
-
-create or alter view vw_taxa_de_transferência as
-select
-  fk_servidor,
-  avg(taxa_de_transferência) as taxa_de_transferência,
-  data_hora
-from vw_registro
-group by data_hora, fk_servidor
-go
-
-
-create or alter view vw_pacotes_enviados as
-select
-  fk_servidor,
-  avg(pacotes_enviados) as pacotes_enviados,
-  data_hora
-from vw_registro
-group by data_hora, fk_servidor
-go
